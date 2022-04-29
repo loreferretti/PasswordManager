7c75a84a241afbad38023afc3f7ff479443eb926
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:14:54 +0200

    add Login class

--- src/main/java/it/passwordmanager/domanModel/Login.java
+++ src/main/java/it/passwordmanager/domanModel/Login.java
@@ -1,2 +1,37 @@
-package it.passwordmanager.domanModel;public class Login {
+package it.passwordmanager.domanModel;
+
+public class Login {
+    private String website;
+    private String username;
+    private String password;
+
+    public Login(String website, String username, String password) {
+        this.website = website;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
