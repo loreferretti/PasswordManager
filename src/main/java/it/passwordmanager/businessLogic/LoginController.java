35b980f6dc4b2b6185a419dfe1f6a0714896f686
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:10:59 +0200

    add LoginController class

---
+++ src/main/java/it/passwordmanager/businessLogic/LoginController.java
@@ -0,0 +1,31 @@
+package it.passwordmanager.businessLogic;
+
+import it.passwordmanager.dao.Dao;
+import it.passwordmanager.domanModel.Login;
+
+import java.util.List;
+
+public class LoginController {
+
+    private Dao<Login> proxy;
+
+    public boolean addLogin(Login login) {
+        return false;
+    }
+
+    public boolean updateLogin(Login login) {
+        return false;
+    }
+
+    public boolean deleteLogin(Login login) {
+        return false;
+    }
+
+    public List<Login> searchByWebsite(String website) {
+        return null;
+    }
+
+    public boolean encrypt(int cipherMode) {
+        return false;
+    }
+}
