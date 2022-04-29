588b1f2346ffc2a8120c609560e8525539d4c3b5
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:14:26 +0200

    add Dao interface

--- src/main/java/it/passwordmanager/dao/Dao.java
+++ src/main/java/it/passwordmanager/dao/Dao.java
@@ -1,2 +1,13 @@
-package it.passwordmanager.dao;public class Dao {
+package it.passwordmanager.dao;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface Dao<T>{
+    public List<T> getAll();
+    public boolean create(T t);
+    public List<T> read(Predicate predicate);
+    public boolean update(T t);
+    public boolean delete(T t);
+
 }
