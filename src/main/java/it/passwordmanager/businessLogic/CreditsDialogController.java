566660608b2f3ca543642a2fa0c4a164af0f9b94
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-04-29 20:21:12 +0200

    add onCloseButtonClick method

--- src/main/java/it/passwordmanager/businessLogic/CreditsDialogController.java
+++ src/main/java/it/passwordmanager/businessLogic/CreditsDialogController.java
@@ -1,4 +1,19 @@
 package it.passwordmanager.businessLogic;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class CreditsDialogController {
+
+    @FXML
+    protected void onCloseButtonClick(ActionEvent event) {
+
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        stage.close();
+
+    }
+
 }
