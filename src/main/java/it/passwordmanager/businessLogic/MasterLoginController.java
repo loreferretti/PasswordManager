062b43db4103204fb89f83ab8636fac50a713dc0
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-04-29 20:24:22 +0200

    add onEnterButtonClick and initialize methods

--- src/main/java/it/passwordmanager/businessLogic/MasterLoginController.java
+++ src/main/java/it/passwordmanager/businessLogic/MasterLoginController.java
@@ -1,4 +1,75 @@
 package it.passwordmanager.businessLogic;
 
-public class MasterLoginController {
+import it.passwordmanager.Launch;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MasterLoginController implements Initializable {
+
+    private LoginController loginController;
+
+    @FXML
+    private PasswordField password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        loginController = new LoginController();
+
+    }
+
+    @FXML
+    protected void onEnterButtonClick(ActionEvent event) throws IOException {
+
+        String masterPassword = password.getText();
+
+        if(loginController.authenticate(masterPassword)) {
+
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Launch.class.getResource("main_window.fxml"));
+
+            Scene scene = new Scene(fxmlLoader.load());
+
+            stage.setTitle("Password Manager");
+            stage.setScene(scene);
+
+            stage.show();
+            stage.centerOnScreen();
+
+        }
+        else {
+
+            password.clear();
+
+            Stage stage = new Stage();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Launch.class.getResource("wrong_login.fxml"));
+
+            Scene scene = new Scene(fxmlLoader.load());
+
+            stage.setTitle("Wrong Login");
+            stage.setResizable(false);
+            stage.setScene(scene);
+
+            stage.show();
+            stage.centerOnScreen();
+
+        }
+
+    }
+
+
+
+
 }
