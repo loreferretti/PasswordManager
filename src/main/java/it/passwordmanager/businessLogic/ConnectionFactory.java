ea9306b80924b9c653c3c95d46e0a0184c8550a9
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:12:03 +0200

    add ConnectionFactory class

---
+++ src/main/java/it/passwordmanager/businessLogic/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package it.passwordmanager.businessLogic;
+
+import java.sql.Connection;
+public class ConnectionFactory {
+    private static final String URL = "";
+
+    public static Connection getConnection() {
+        return null;
+    }
+}
