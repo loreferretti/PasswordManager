8ac07c880273700dcbc08a5b3ae68210018c21b6
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:11:38 +0200

    add EncryptionService class

---
+++ src/main/java/it/passwordmanager/businessLogic/EncryptionService.java
@@ -0,0 +1,16 @@
+package it.passwordmanager.businessLogic;
+
+public class EncryptionService {
+    public byte[] getEncryptedPassword(String password, byte[] salt) {
+        return null;
+    }
+
+    public byte[] generateSalt() {
+        return null;
+    }
+
+    public void DbEncryption(int cipherMode) {
+
+    }
+
+}
