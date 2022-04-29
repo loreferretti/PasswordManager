84b78be614422c2b8e3fc1380a534d91cfb526eb
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:13:20 +0200

    add Launch class

--- src/main/java/it/passwordmanager/Launch.java
+++ src/main/java/it/passwordmanager/Launch.java
@@ -1,2 +1,20 @@
-package it.passwordmanager;public class Launch {
+package it.passwordmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launch extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
