61c51273a7a7e21712a2bdaf406b9f0cf10721cb
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-29 18:14:42 +0200

    override Dao interface methods

--- src/main/java/it/passwordmanager/dao/LoginDao.java
+++ src/main/java/it/passwordmanager/dao/LoginDao.java
@@ -1,2 +1,34 @@
-package it.passwordmanager.dao;public class LoginDao {
+package it.passwordmanager.dao;
+
+import it.passwordmanager.domanModel.Login;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LoginDao implements Dao<Login> {
+
+    @Override
+    public List<Login> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean create(Login login) {
+        return false;
+    }
+
+    @Override
+    public List<Login> read(Predicate predicate) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Login login) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Login login) {
+        return false;
+    }
 }
