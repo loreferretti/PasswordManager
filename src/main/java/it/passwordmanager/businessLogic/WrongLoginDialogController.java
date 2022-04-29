ac8b5c6f699d2b5ba674e09e4b58ee507c4e14f7
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-04-29 20:21:25 +0200

    add onRetryButtonClick method and Constructor

--- src/main/java/it/passwordmanager/businessLogic/WrongLoginDialogController.java
+++ src/main/java/it/passwordmanager/businessLogic/WrongLoginDialogController.java
@@ -1,4 +1,19 @@
 package it.passwordmanager.businessLogic;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class WrongLoginDialogController {
+
+    @FXML
+    protected void onRetryButtonClick(ActionEvent event) {
+
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        stage.close();
+
+    }
+
 }
