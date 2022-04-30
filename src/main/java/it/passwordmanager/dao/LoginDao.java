64e0ee3fde2ef4e2f57f5f1ab1eca8cd3db55206
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 16:45:04 +0200

    implement getAll method

--- src/main/java/it/passwordmanager/dao/LoginDao.java
+++ src/main/java/it/passwordmanager/dao/LoginDao.java
@@ -1,7 +1,13 @@
 package it.passwordmanager.dao;
 
+import it.passwordmanager.businessLogic.ConnectionFactory;
 import it.passwordmanager.domainModel.Login;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -9,7 +15,26 @@ public class LoginDao implements Dao<Login> {
 
     @Override
     public List<Login> getAll() {
-        return null;
+        Connection connection = ConnectionFactory.getConnection();
+        String query = "select * from Login;";
+        try {
+            PreparedStatement pstat = connection.prepareStatement(query);
+            ResultSet rs = pstat.executeQuery();
+
+            ArrayList<Login>  logins = new ArrayList<Login>();
+            while(rs.next()) {
+                String website = rs.getString("website");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                logins.add(new Login(website,username,password));
+            }
+            return logins;
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
