b588b639585dc0bb93811de9b678f09ee2a58d39
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 16:28:45 +0200

    add method for creating alphabet password

--- src/main/java/it/passwordmanager/businessLogic/PasswordGenerator.java
+++ src/main/java/it/passwordmanager/businessLogic/PasswordGenerator.java
@@ -1,8 +1,24 @@
 package it.passwordmanager.businessLogic;
 
+import java.util.Random;
+
 public class PasswordGenerator {
 
     public String generate(int length) {
         return "";
+    }
+
+    private String getAlphabet() {
+        String alphabet = "";
+        for (char c = 'A'; c <= 'Z'; c++)
+            alphabet += c;
+
+        for (char c = 'a'; c <= 'z'; c++)
+            alphabet += c;
+
+        for (int i = 0; i < 10; i++)
+            alphabet += i;
+
+        return alphabet;
     }
 }
