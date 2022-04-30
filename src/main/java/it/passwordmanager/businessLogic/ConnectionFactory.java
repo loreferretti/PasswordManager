e4296c795c0e75bedbdd3321e04a1bf42feba81b
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 16:15:19 +0200

    implement getConnection method

--- src/main/java/it/passwordmanager/businessLogic/ConnectionFactory.java
+++ src/main/java/it/passwordmanager/businessLogic/ConnectionFactory.java
@@ -1,10 +1,17 @@
 package it.passwordmanager.businessLogic;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    private static final String URL = "";
+    private static final String URL = "jdbc:sqlite:db-login.db";
 
     public static Connection getConnection() {
-        return null;
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
     }
 }
