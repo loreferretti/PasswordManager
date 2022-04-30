5d07bf5f45042e7b61f1b79936256533aeae1a83
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 17:23:50 +0200

    implement all methods of the Dao interface by forwarding the call to LoginDao

--- src/main/java/it/passwordmanager/businessLogic/AuthenticationProxy.java
+++ src/main/java/it/passwordmanager/businessLogic/AuthenticationProxy.java
@@ -1,35 +1,36 @@
 package it.passwordmanager.businessLogic;
 
 import it.passwordmanager.dao.Dao;
+import it.passwordmanager.dao.LoginDao;
 import it.passwordmanager.domainModel.Login;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 public class AuthenticationProxy implements Dao<Login> {
+    private LoginDao loginDao;
     @Override
     public List<Login> getAll() {
-        return null;
+        return loginDao.getAll();
     }
 
     @Override
     public boolean create(Login login) {
-        return false;
+        return loginDao.create(login);
     }
 
     @Override
     public List<Login> read(Object obj) {
-        return null;
+        return loginDao.read(obj);
     }
 
     @Override
     public boolean update(Login login) {
-        return false;
+        return loginDao.update(login);
     }
 
     @Override
     public boolean delete(Login login) {
-        return false;
+        return loginDao.delete(login);
     }
 
     public boolean authenticate(String password) {
