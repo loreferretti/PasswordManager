5b01c047ae840f2e5dc4a04b5e5ac475d02ce0e5
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 16:04:49 +0200

    implement generateSalt method

--- src/main/java/it/passwordmanager/businessLogic/EncryptionService.java
+++ src/main/java/it/passwordmanager/businessLogic/EncryptionService.java
@@ -1,12 +1,26 @@
 package it.passwordmanager.businessLogic;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
 public class EncryptionService {
     public byte[] getEncryptedPassword(String password, byte[] salt) {
         return null;
     }
 
     public byte[] generateSalt() {
-        return null;
+        try {
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            byte[] salt = new byte[8];
+            random.nextBytes(salt);
+            return salt;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void DbEncryption(int cipherMode) {
