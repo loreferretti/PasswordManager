478efbe8132a21efb89be838eafc3e40a0818e64
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-04-30 18:14:27 +0200

    implement authenticate method

--- src/main/java/it/passwordmanager/businessLogic/IdentityManager.java
+++ src/main/java/it/passwordmanager/businessLogic/IdentityManager.java
@@ -1,7 +1,28 @@
 package it.passwordmanager.businessLogic;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Properties;
+
 public class IdentityManager {
     public boolean authenticate(String password) {
-        return false;
+        boolean valid = false;
+        try(FileReader reader = new FileReader("src/main/resources/.passwordManager.properties")) {
+            Properties prop = new Properties();
+            prop.load(reader);
+            byte[] masterPassword = Base64.getDecoder().decode(prop.getProperty("masterPassword"));
+            byte[] salt = Base64.getDecoder().decode(prop.getProperty("salt"));
+            valid = checkLogin(password, masterPassword, salt);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return valid;
     }
+
 }
