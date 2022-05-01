b703fb09e1772a31696d6f13ec0e698116cfbfad
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-05-01 12:45:33 +0200

    add class fields and initialize method

--- src/main/java/it/passwordmanager/businessLogic/DeleteConfirmDialogController.java
+++ src/main/java/it/passwordmanager/businessLogic/DeleteConfirmDialogController.java
@@ -1,4 +1,27 @@
 package it.passwordmanager.businessLogic;
 
+import it.passwordmanager.domainModel.Login;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class DeleteConfirmDialogController {
+
+    private LoginController loginController;
+    private Login login;
+
+    private ShowLoginController showLoginController;
+
+    public void initialize(ShowLoginController parentController, Login login) {
+        this.login = login;
+
+        loginController = new LoginController();
+
+        this.showLoginController = parentController;
+
+    }
+
+
+
 }
