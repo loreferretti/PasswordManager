ab1a6d1d6f4020b8befb2968ae861611aabc4424
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-05-01 12:42:01 +0200

    add class fields and two intialize method

--- src/main/java/it/passwordmanager/businessLogic/BackWithoutSaveConfirmDialogController.java
+++ src/main/java/it/passwordmanager/businessLogic/BackWithoutSaveConfirmDialogController.java
@@ -1,4 +1,32 @@
 package it.passwordmanager.businessLogic;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class BackWithoutSaveConfirmDialogController {
+
+    private LoginController loginController;
+
+    private AddLoginController addLoginControllerParent;
+    private EditLoginController editLoginControllerParent;
+
+    public void initialize(AddLoginController parentController) {
+
+        loginController = new LoginController();
+
+        this.addLoginControllerParent = parentController;
+
+    }
+
+    public void initialize(EditLoginController parentController) {
+
+        loginController = new LoginController();
+
+        this.editLoginControllerParent = parentController;
+
+    }
+
+
 }
