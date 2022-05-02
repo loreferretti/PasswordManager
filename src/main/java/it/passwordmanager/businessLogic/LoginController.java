223f3c0bc69c97e7aacd4e37a7f4fda9b1f5bcfc
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-02 22:08:24 +0200

    add and implement onStart method for decrypting file .db

--- src/main/java/it/passwordmanager/businessLogic/LoginController.java
+++ src/main/java/it/passwordmanager/businessLogic/LoginController.java
@@ -3,6 +3,8 @@ package it.passwordmanager.businessLogic;
 import it.passwordmanager.dao.Dao;
 import it.passwordmanager.domainModel.Login;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class LoginController {
@@ -33,6 +35,20 @@ public class LoginController {
 
     public boolean encrypt(int cipherMode) {
         return false;
+    }
+
+    public void onStart(String masterPassword) {
+        try {
+            File db = new File("db-login-encrypted.db");
+            File db_temp = File.createTempFile("db-login",".db");
+            db_temp.deleteOnExit();
+
+            EncryptionService es = new EncryptionService();
+            es.DbEncryption(2, es.padding(masterPassword).toString() , db, db_temp);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean storeAndEncryptPassword(String password) {
