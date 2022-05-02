3db00d6a48a7db0747b0a07a6f5b66fdc21170c5
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-05-02 12:32:22 +0200

    Remove of one of the initialize methods

--- src/main/java/it/passwordmanager/businessLogic/BackWithoutSaveOnAddConfirmDialogController.java
+++ src/main/java/it/passwordmanager/businessLogic/BackWithoutSaveOnAddConfirmDialogController.java
@@ -5,26 +5,17 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
-public class BackWithoutSaveConfirmDialogController {
+public class BackWithoutSaveOnAddConfirmDialogController {
 
     private LoginController loginController;
 
     private AddLoginController addLoginControllerParent;
-    private EditLoginController editLoginControllerParent;
 
     public void initialize(AddLoginController parentController) {
 
         loginController = new LoginController();
 
         this.addLoginControllerParent = parentController;
-
-    }
-
-    public void initialize(EditLoginController parentController) {
-
-        loginController = new LoginController();
-
-        this.editLoginControllerParent = parentController;
 
     }
 
