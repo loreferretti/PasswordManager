3017e8f47063b1f6f214e3a74ebde1cdd263a102
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-03 21:28:18 +0200

    test getConnection method

--- src/test/it/passwordmanager/businessLogic/ConnectionFactoryTest.java
+++ src/test/it/passwordmanager/businessLogic/ConnectionFactoryTest.java
@@ -1,20 +1,32 @@
 package it.passwordmanager.businessLogic;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectionFactoryTest {
-    @BeforeAll
-    void setup() {
-        File db = new File("src/test/db-login-test.db");
 
-    }
+    @Rule
+    TemporaryFolder folder= new TemporaryFolder();
 
     @Test
-    void getConnection() {
+    void getConnection() throws IOException, SQLException {
+        folder.create();
+        File db = folder.newFile("db-login-test.db");
+        String path = "jdbc:sqlite:db-login-test.db";
+        Connection connection = ConnectionFactory.getConnection(path);
+        assertNotNull(connection);
+
+
+
     }
 }
