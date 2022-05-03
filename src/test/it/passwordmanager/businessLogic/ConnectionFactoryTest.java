00ca0d1d84fad4bab514adc850a5e5f3b3fbe4e1
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-03 20:32:12 +0200

    create ConnectionFactoryTest class

--- src/test/it/passwordmanager/businessLogic/ConnectionFactoryTest.java
+++ src/test/it/passwordmanager/businessLogic/ConnectionFactoryTest.java
@@ -1,10 +1,20 @@
 package it.passwordmanager.businessLogic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectionFactoryTest {
+    @BeforeAll
+    void setup() {
+        File db = new File("src/test/db-login-test.db");
 
+    }
 
+    @Test
+    void getConnection() {
+    }
 }
