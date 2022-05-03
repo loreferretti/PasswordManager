37b45b9f7c9c71f3c0d31382f66aeeed1d665650
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-03 12:55:06 +0200

    add and implement storePassword method

--- src/main/java/it/passwordmanager/businessLogic/IdentityManager.java
+++ src/main/java/it/passwordmanager/businessLogic/IdentityManager.java
@@ -1,8 +1,6 @@
 package it.passwordmanager.businessLogic;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Properties;
@@ -10,19 +8,36 @@ import java.util.Properties;
 public class IdentityManager {
     public boolean authenticate(String password) {
         boolean valid = false;
-        try(FileReader reader = new FileReader("src/main/resources/.passwordManager.properties")) {
+        File fileProperties = new File("src/main/resources/.passwordManager.properties");
+        try(FileReader reader = new FileReader(fileProperties)) {
             Properties prop = new Properties();
             prop.load(reader);
-            byte[] masterPassword = Base64.getDecoder().decode(prop.getProperty("masterPassword"));
-            byte[] salt = Base64.getDecoder().decode(prop.getProperty("salt"));
-            valid = checkLogin(password, masterPassword, salt);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            if(prop.getProperty("masterPassword") != null && prop.getProperty("salt") != null) {
+                byte[] masterPassword = Base64.getDecoder().decode(prop.getProperty("masterPassword"));
+                byte[] salt = Base64.getDecoder().decode(prop.getProperty("salt"));
+                valid = checkLogin(password, masterPassword, salt);
+            } else {
+                storePassword(prop, fileProperties, password);
+                valid = true;
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         return valid;
+    }
+
+    private void storePassword(Properties prop, File fileProperties, String password) {
+        try(FileOutputStream fos = new FileOutputStream(fileProperties)) {
+            EncryptionService es = new EncryptionService();
+            byte[] salt = es.generateSalt();
+            prop.setProperty("masterPassword", Base64.getEncoder().encodeToString(es.getEncryptedPassword(password, salt)));
+            prop.setProperty("salt", Base64.getEncoder().encodeToString(salt));
+            prop.store(fos, null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     private boolean checkLogin(String password, byte[] masterPassword, byte[] salt) {
