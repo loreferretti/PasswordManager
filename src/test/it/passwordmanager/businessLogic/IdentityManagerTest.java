babb27c2bd98e9e4f45df2d351f01c728c3c4aba
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-03 20:33:28 +0200

    create IdentityManagerTest class for testing authenticate method

--- src/test/it/passwordmanager/businessLogic/IdentityManagerTest.java
+++ src/test/it/passwordmanager/businessLogic/IdentityManagerTest.java
@@ -1,12 +1,28 @@
 package it.passwordmanager.businessLogic;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IdentityManagerTest {
 
+    private final String URL = "passwordManagerTest.properties";
+
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
     @Test
-    void authenticate() {
+    public void authenticate() throws IOException {
+        folder.create();
+        File fileProp = folder.newFile(URL);
+        IdentityManager im = new IdentityManager(fileProp.getPath());
+        String password = "myPassword";
+        im.authenticate(password);
+        assertTrue(im.authenticate(password));
     }
 }
