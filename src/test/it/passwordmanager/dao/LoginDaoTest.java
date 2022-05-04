0f4e11afb3a5823601495d450f53dabc3cecf170
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-04 03:04:26 +0200

    test all methods of LoginDao

--- src/test/it/passwordmanager/dao/LoginDaoTest.java
+++ src/test/it/passwordmanager/dao/LoginDaoTest.java
@@ -3,18 +3,15 @@ package it.passwordmanager.dao;
 import it.passwordmanager.businessLogic.ConnectionFactory;
 import it.passwordmanager.businessLogic.EncryptionService;
 import it.passwordmanager.domainModel.Login;
-import org.junit.Before;
+import org.junit.jupiter.api.*;
 import org.junit.Rule;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,20 +20,19 @@ class LoginDaoTest {
 
     final String dbName = "db-login-test.db";
     final String dbPath = String.format("jdbc:sqlite:%s", dbName);
-    String password = "MyPassword";
+    final String password = "MyPassword";
+    final String paddedPassword = new String(EncryptionService.padding(password));
 
     @Rule
-    TemporaryFolder folder= new TemporaryFolder();
+    static TemporaryFolder folder= new TemporaryFolder();
 
-    @Before
-    void setupDb() throws IOException, SQLException {
+    @BeforeEach
+    void init() throws IOException, SQLException {
         folder.create();
-        File db = folder.newFile(dbName);
-        String path = "jdbc:sqlite:db-login-test.db";
+        folder.newFile(dbName);
         Connection connection = ConnectionFactory.getConnection(dbPath);
         if(connection != null) {
-            DatabaseMetaData meta = connection.getMetaData();
-            String query = "CREATE TABLE \"Login\" (\n" +
+            String queryTable = "create table if not exists\"Login\" (\n" +
                     " \"id\" INTEGER,\n" +
                     " \"website\" TEXT NOT NULL,\n" +
                     " \"username\" TEXT NOT NULL,\n" +
@@ -44,35 +40,81 @@ class LoginDaoTest {
                     " UNIQUE(\"website\",\"username\",\"password\"),\n" +
                     " PRIMARY KEY(\"id\" AUTOINCREMENT)\n" +
                     ");";
-            PreparedStatement pstat = connection.prepareStatement(query);
-            pstat.execute();
+            PreparedStatement pstatTable = connection.prepareStatement(queryTable);
+            pstatTable.execute();
+
+            String query = "insert into Login (website, username, password) values (?,?,?)";
+            PreparedStatement pstatInsert = connection.prepareStatement(query);
+            pstatInsert.setString(1, EncryptionService.encrypt(paddedPassword, "someWebsite"));
+            pstatInsert.setString(2, EncryptionService.encrypt(paddedPassword, "someUsername"));
+            pstatInsert.setString(3, EncryptionService.encrypt(paddedPassword, "somePassword"));
+            pstatInsert.execute();
         }
 
     }
 
     @Test
-    void createAndReadAll() {
+    void getAll() {
         LoginDao loginDao = new LoginDao(dbPath);
         List<Login> logins;
-        Login login = new Login("someWebsite", "someUsername", "somePassword");
+        logins = loginDao.getAll(password);
+        assertEquals(1, logins.size());
+        assertEquals(logins.get(0).getWebsite(), "someWebsite");
+        assertEquals(logins.get(0).getUsername(), "someUsername");
+        assertEquals(logins.get(0).getPassword(), "somePassword");
+
+    }
+
+    @Test
+    void create() {
+        LoginDao loginDao = new LoginDao(dbPath);
+        Login login = new Login("anotherWebsite", "anotherUsername", "anotherPassword");
+        List<Login> logins;
         if(loginDao.create(password, login)) {
             logins = loginDao.getAll(password);
-            assertEquals(1, logins.size());
-            assertEquals(logins.get(0), login);
+            assertEquals(2, logins.size());
+            assertEquals(logins.get(0).getWebsite(), "someWebsite");
+            assertEquals(logins.get(0).getUsername(), "someUsername");
+            assertEquals(logins.get(0).getPassword(), "somePassword");
+            assertEquals(logins.get(1).getWebsite(), "anotherWebsite");
+            assertEquals(logins.get(1).getUsername(), "anotherUsername");
+            assertEquals(logins.get(1).getPassword(), "anotherPassword");
         }
-
 
     }
 
     @Test
     void read() {
+        LoginDao loginDao = new LoginDao(dbPath);
+        loginDao.getAll(password);
+        String toSearch = "some";
+        List<Login> logins;
+        logins = loginDao.read(toSearch);
+        assertEquals(1, logins.size());
+        assertEquals(logins.get(0).getWebsite(), "someWebsite");
+        assertEquals(logins.get(0).getUsername(), "someUsername");
+        assertEquals(logins.get(0).getPassword(), "somePassword");
+
     }
 
     @Test
     void update() {
+
     }
 
     @Test
     void delete() {
+    }
+
+    @AfterEach
+    void teardown() throws SQLException {
+        Connection connection = ConnectionFactory.getConnection(dbPath);
+        if(connection != null) {
+            String queryDropTable = "drop table Login;";
+            PreparedStatement pstat = connection.prepareStatement(queryDropTable);
+            pstat.execute();
+        }
+        //FIXME doesnt't delete file
+        folder.delete();
     }
 }
