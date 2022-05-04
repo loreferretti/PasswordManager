558618ac3782eba0454b55bfe23060e57cbd7e49
Tommaso Gualtierotti <tommaso.gualtierotti@stud.unifi.it>
2022-05-04 12:06:55 +0200

    removal of unused import statement

--- src/main/java/it/passwordmanager/Launch.java
+++ src/main/java/it/passwordmanager/Launch.java
@@ -1,10 +1,8 @@
 package it.passwordmanager;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 
 import java.io.File;
