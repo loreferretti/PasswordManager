c28b6eda0207b88c765831444f4e478e58e5cd23
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-04 19:06:29 +0200

    add folder delete

--- src/test/it/passwordmanager/businessLogic/IdentityManagerTest.java
+++ src/test/it/passwordmanager/businessLogic/IdentityManagerTest.java
@@ -24,5 +24,6 @@ public class IdentityManagerTest {
         String password = "myPassword";
         im.authenticate(password);
         assertTrue(im.authenticate(password));
+        folder.delete();
     }
 }
