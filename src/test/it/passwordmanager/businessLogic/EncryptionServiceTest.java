f87ce11a1f4b31c2cd3ddcd7419fcc101f39ddcc
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-05-04 13:20:52 +0200

    add test getEncryptedPassword method

--- src/test/it/passwordmanager/businessLogic/EncryptionServiceTest.java
+++ src/test/it/passwordmanager/businessLogic/EncryptionServiceTest.java
@@ -2,6 +2,8 @@ package it.passwordmanager.businessLogic;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EncryptionServiceTest {
@@ -12,5 +14,13 @@ public class EncryptionServiceTest {
         String field = "This is the field to be encrypt";
         String fieldEncrypted = EncryptionService.encrypt(keyVal, field);
         assertEquals(field, EncryptionService.decrypt(keyVal,fieldEncrypted));
+    }
+
+    @Test
+    public void getEncryptedPassword() {
+        String password = "test";
+        String passwordEncrypted = "u2fIA1kfob3H7igSdNiB+e+R6sk=";
+        byte[] salt = Base64.getDecoder().decode("eOOJqULjj6U=");
+        assertEquals(passwordEncrypted, Base64.getEncoder().encodeToString(EncryptionService.getEncryptedPassword(password,salt)));
     }
 }
